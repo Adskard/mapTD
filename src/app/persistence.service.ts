cd70e06da3d77307794016d63d7bce7c94748abd
Adam Škarda <adskard@adskard>
2023-05-11 09:48:56 +0200

    new files

---
+++ src/app/persistence.service.ts
@@ -0,0 +1,101 @@
+import { Injectable } from '@angular/core';
+import { openDB, deleteDB, wrap, unwrap, DBSchema } from 'idb';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PersistenceService {
+
+  db_name = "leaderboard"
+  score_index = "score_idx"
+
+  getTopLeaders = async () : Promise<leaderboardItem[] | undefined>  =>  {
+    console.log("Getting Leaders")
+  
+    const db = await openDB<dbSchema>("db", 1, {
+      upgrade(db, oldVersion, newVersion, transaction, event) {
+        console.log("upgrading")
+        if(!db.objectStoreNames.contains("leaderboard")){
+          console.log("upgrading db")
+          const store =  db.createObjectStore("leaderboard", {autoIncrement: true, keyPath: 'key'});
+          store.createIndex("by-score", "score");
+        }
+      },
+      blocked(currentVersion, blockedVersion, event) {
+        console.log("Db blocked")
+      },
+      blocking(currentVersion, blockedVersion, event) {
+        console.log("Db blocking")
+      },
+      terminated() {
+        console.log("Db terminated")
+      },
+    });
+    
+
+    try{
+      await db.add("leaderboard", {name:"adam", score: 20});
+      await db.add("leaderboard", {name:"adam", score: 19});
+      await db.add("leaderboard", {name:"adam", score: 18});
+      await db.add("leaderboard", {name:"adam", score: 21});
+      const top_leaders = await db.getAllFromIndex("leaderboard", "by-score", IDBKeyRange.lowerBound(1))
+      return top_leaders;
+    }
+    catch(err){
+      console.error(err)
+    }
+    return undefined;
+  } 
+
+
+  addLeader = async (item : leaderboardItem) : Promise<void>  =>  {
+    console.log("Adding leader")
+  
+    const db = await openDB<dbSchema>("db", 1, {
+      upgrade(db, oldVersion, newVersion, transaction, event) {
+        console.log("upgrading")
+        if(!db.objectStoreNames.contains("leaderboard")){
+          console.log("upgrading db")
+          const store =  db.createObjectStore("leaderboard", {autoIncrement: true, keyPath: 'key'});
+          store.createIndex("by-score", "score");
+        }
+      },
+      blocked(currentVersion, blockedVersion, event) {
+        console.log("Db blocked")
+      },
+      blocking(currentVersion, blockedVersion, event) {
+        console.log("Db blocking")
+      },
+      terminated() {
+        console.log("Db terminated")
+      },
+    });
+    
+  
+    try{
+      await db.add("leaderboard", item);
+    }
+    catch(err){
+      console.error(err)
+    }
+  } ;
+
+  constructor() { }
+}
+
+
+
+
+interface dbSchema extends DBSchema {
+
+  leaderboard: {
+    value: leaderboardItem;
+    key: number;
+    indexes: { 'by-score': number };
+  };
+}
+
+export interface leaderboardItem{
+  name: String;
+  score: number;
+}
