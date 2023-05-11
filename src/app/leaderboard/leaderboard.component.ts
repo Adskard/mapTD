5ab84215f006b38c34f680981777828b3cd4a7f6
Adam Škarda <adskard@adskard>
2023-05-11 09:48:29 +0200

    base look, db usable

--- src/app/leaderboard/leaderboard.component.ts
+++ src/app/leaderboard/leaderboard.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { PersistenceService, leaderboardItem } from '../persistence.service';
 
 @Component({
   selector: 'app-leaderboard',
@@ -6,5 +7,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./leaderboard.component.scss']
 })
 export class LeaderboardComponent {
+  
+  leaderboardItems : Array<leaderboardItem> = []
+
+  leaderCount = 10
+
+  public ngAfterViewInit() : void {
+    this.persistence.getTopLeaders()
+    .then((p)=>{
+      console.log(p)
+      if(p)
+      this.leaderboardItems = p.reverse().slice(0, this.leaderCount);
+    });
+  }
+
+  private updateLeaderBoard(newItems : Array<leaderboardItem>){
+    this.leaderboardItems = newItems;
+  }
+  //inject dependencies
+  constructor(private persistence : PersistenceService){
+  };
 
 }
